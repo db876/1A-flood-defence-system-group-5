73604b20e83e4a4e8cc03d707565732fd41f6931
db876 <98701336+db876@users.noreply.github.com>
2022-01-31 23:14:50 +0000

    Task 1B v1

--- floodsystem/geo.py
+++ floodsystem/geo.py
@@ -6,4 +6,17 @@ geographical data.
 
 """
 
-from .utils import sorted_by_key  # noqa
+from floodsystem.utils import sorted_by_key  # noqa
+from floodsystem.station import MonitoringStation
+import haversine
+
+def stations_by_distance(stations, p):
+    '''This function is required for task 1B'''
+    '''This function returns a list of stations and there distance from an arbitrary point p'''
+    stations_with_distance = []
+    for i in range(len(stations)):
+        distance = float(haversine(p, stations[i].coord))
+        stations_with_distance.append(stations[i].name, stations[i].town, distance)
+    stations_with_distance = sorted_by_key(stations_with_distance, 2, reverse=False)
+    return stations_with_distance
+
